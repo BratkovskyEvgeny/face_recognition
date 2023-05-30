b6bd99ef5a8b328ba61a07dd9614d697f7659037
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-05-31 00:50:11 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -132,8 +132,8 @@ def attendance(id, name):
         df_attendace = pd.read_csv(f_p)
        
        # df_attendace = df_attendace.append(df_attendace_temp)  
-        
-        df_attendace =df_attendace.loc[len(df_attendace)] 
+        df_attendace = pd.concat([df_attendace,  df_attendace_temp],ignore_index=True)
+        #df_attendace =df_attendace.loc[len(df_attendace)] 
         #df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
         df_attendace.to_csv(f_p, index=False)
         # st.write(df_attendace)
