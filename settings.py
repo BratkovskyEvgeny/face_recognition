903a5fe744ea8e24de09748434a10cc722253865
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-05-31 00:09:12 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os, datetime, json, sys, pathlib, shutil
 import pandas as pd
 import streamlit as st
-import cv2
+from cv2 import cv2
 import face_recognition
 import numpy as np
 from PIL import Image
