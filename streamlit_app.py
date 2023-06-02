3187619c55f6a2617502f3fe614b785e37d6db3e
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-06-02 16:08:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 #page_bg_img = f"""
 #<style>
 #[data-testid="stAppViewContainer"] > .main {{
-#background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+#background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");  
 ##background-size: 180%;
 #background-position: top left;
 #background-repeat: no-repeat;
