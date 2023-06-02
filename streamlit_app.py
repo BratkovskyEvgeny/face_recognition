bfbdecdc1c8558ec7d92aa482ae61fc9e3d7f228
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-06-02 16:12:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,22 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_option('deprecation.showfileUploaderEncoding', False)
 ## Defining Static Data
 
-
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('face3.jpg')    
 #@st.cache_data
 #def get_img_as_base64(file):
    # with open(file, "rb") as f:
